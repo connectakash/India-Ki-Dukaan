12ac15d44b1e0536bcd8440b8f71d876d4c43f00
connectakash <akashprajasekharan@gmail.com>
2019-02-09 14:12:22 +0530

    added node modules added and addproduct backend and angular added

--- backend/addproduct.js
+++ backend/addproduct.js
@@ -1,13 +1,25 @@
 let app=angular.module("myApp",[]);
-
 app.controller('addproduct',function($scope,$http){
     console.log("inside controller");
     $scope.submit=function(){
             console.log("inside submit");
             let data={
-                "SellerName":$scope.SellerName,
+                //"SellerName":$scope.Sellername,
+                
+                "ProductName":$scope.Product,
+                "ProductCategory":$scope.Category,
+                "AboutProduct":$scope.aboutProduct,
+                "Description":$scope.Description,
+                "Specification":$scope.Specification,
+                "Price":$scope.Price,
+                "Quantity":$scope.Quantity,
+                "ImageUrl":$scope.ImageURL,
+                "SellerId":1
 
             };
             console.log(data);
-    };
+            $http.post("http://localhost:3000/add",data).then(function(response){
+                console.log("data request sent");
+            });
+    }
 });
